cd0a29bb7e89d0ad7d7f683c87ab449c8270bac4
s64309010023 <s64309010023@ptnicec.ac.th>
2022-10-20 11:14:20 +0700

    ไฟล์ variable/test

--- test.php
+++ test.php
@@ -1,5 +1,25 @@
 <?php
 
-
-
+    echo "Hasanee Kalatae<br>";
+    //echo "Thailand";
+    #echo "Asia";
+    /*
+    echo "world<br>";
+    echo "Universe<br>";
+    */
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST PHP</title>
+</head>
+<body>
+    Information Technology
+    <?php
+        echo "Pattani;"
+    ?>  
+</body>
+</html>
