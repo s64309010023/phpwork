7db31285cc4c1ff47f911d7bf3eb225c5c5dbb5f
s64309010023 <s64309010023@ptnicec.ac.th>
2022-10-20 12:15:03 +0700

    ส่ง datatype

---
+++ datatype.php
@@ -0,0 +1,27 @@
+<?php
+
+    //String
+    $grade = "A";
+    $subject = "HTML";
+    $name = 'Hasanee';
+    $age = "22";
+
+    //Integer Number
+    $timeout = 30;
+    $score = 99;
+    $lone = -19;
+    $money = +88;
+
+    //Float point
+    $gpa = 3.33;
+    $inits = 0.25;
+    $temp = -2.22;
+    $lumen = +7600.99;
+    $maths = 2e10;
+    echo $maths;
+
+    //Boolean
+    $famale = true;
+    $male = false;
+
+?>
